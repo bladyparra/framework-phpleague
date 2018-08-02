0e781ab9243590706743f5405fbd1173969a8656
Bladimir Gerson Parra Bermudez <ipproyectossoluciones@gmail.com>
2018-08-02 18:58:33 -0500

    Se crea el model User con Eloquent, acceso a base de datos

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
   namespace Application\Controllers;
 
+  use Application\Models\User;
   use Application\Services\View;
 
   class HomeController extends BaseController {
@@ -15,5 +16,23 @@
     {
       $user = 'Bparra';
       return $this->view->render('home.twig', compact('user'));
+    }
+
+    public function findUser($request, $response, $args)
+    {
+      $user = User::find($args['id']);
+      return $this->view->render('user.twig', compact('user'));
+    }
+
+    public function randomUser($request, $response)
+    {
+      $user = User::random();
+      return $this->view->render('user.twig', compact('user'));
+    }
+
+    public function users($request, $response)
+    {
+      $users = User::all();
+      return $this->view->render('users.twig', compact('users'));
     }
   }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@
 
   $route->group('/', function (\League\Route\RouteGroup $route) use($container) {
     $route->map('GET', '/','Application\Controllers\HomeController::index');
+    $route->map('GET', '/user/{id}', 'Application\Controllers\HomeController::findUser');
+    $route->map('GET', '/user_random', 'Application\Controllers\HomeController::randomUser');
+    $route->map('GET', '/users', 'Application\Controllers\HomeController::users');
     $route->map('GET', '/profile/{name}/{age}', 'Application\Controllers\ProfileController::index')
           ->middleware($container->get(Auth::class));
   });
